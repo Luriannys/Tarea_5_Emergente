c3e2ffadc5f76fdb6d77f41be0d52245d1d83e02
Luriannys <ljunco@correo.unimet.edu.ve>
2026-06-22 19:32:51 -0400

    mostrar y agregar zona

--- simulacion.py
+++ simulacion.py
@@ -1,9 +1,29 @@
-import csv
+import pandas as pd
 
-def mostrar_city():
-    with open ('city.csv', mode= 'r', newline='') as file:
-        spamreader = csv.reader(file, delimiter=';', quotechar='|')
-        for row in spamreader:
-            print(' '.join(row))
+def mostrar_city(df: pd.DataFrame):
+    print(df.to_string())
 
-mostrar_city()
+def agregar_zona(df: pd.DataFrame, tipo: str, inicio_x: str, inicio_y: int):
+    tipo = tipo * 3
+    columnas = list(df.columns)
+    ind_col_base= columnas.index(inicio_x)
+
+    columna = columnas[ind_col_base]
+    for i in range(3):
+        df.loc[inicio_y+i, columna] = tipo
+
+
+city_df = pd.read_csv('city.csv', index_col=0)
+mostrar_city(city_df)
+city_df = city_df.fillna("   ")
+agregar_zona(city_df, 'r', "AB", 10)
+
+mostrar_city(city_df)
+
+agregar_zona(city_df, 'i', "J", 13)
+
+mostrar_city(city_df)
+
+agregar_zona(city_df, 'c', "T", 2)
+
+mostrar_city(city_df)
